f4f886b55723c410a3f726212d89e2c48abd15c6
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2019-11-10 12:14:02 -0200

    Enabling eureka client

--- src/main/java/br/com/maisvida/camunda/MailSenderApplication.java
+++ src/main/java/br/com/maisvida/camunda/MailSenderApplication.java
@@ -4,12 +4,14 @@ import java.util.concurrent.Executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableAsync
+@EnableEurekaClient
 public class MailSenderApplication {
 
 	@Bean
